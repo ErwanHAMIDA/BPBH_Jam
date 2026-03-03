4883d6ac3ae7cdf65431699d75af8fa21333472c
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2026-03-03 23:17:42 +0100

    [ADD] Player Push Object with rigidbody

--- Assets/_Scripts/S_Movement.cs
+++ Assets/_Scripts/S_Movement.cs
@@ -1,25 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class S_Movement : MonoBehaviour
 {
     [SerializeField] InputActionReference _moveAction;
     [Range(1.0f, 100.0f)]
     [SerializeField] float _speed = 20.0f;
+    [Range(-30.0f, 30.0f)]
+    [SerializeField] float _gravity = -9.81f;
+
+    [SerializeField] CharacterController _controller;
 
     private Vector2 _direction;
+    private float _verticalVelocity;
 
-    private void Awake()
-    {
-    }
     private void OnEnable()
     {
         _moveAction.action.Enable();
         _moveAction.action.performed += OnMovementStarted;
         _moveAction.action.canceled += OnMovementCanceled;
     }
-
-    
 
     private void OnDisable()
     {
@@ -41,7 +42,16 @@ public class S_Movement : MonoBehaviour
 
     private void Update()
     {
-        Vector3 test = new Vector3(_direction.x, 0, _direction.y); 
-        transform.Translate(test * _speed * Time.deltaTime);
+        if (_controller.isGrounded && _verticalVelocity < 0)
+        {
+            _verticalVelocity = -2f;
+        }
+
+        _verticalVelocity += _gravity * Time.deltaTime;
+
+        Vector3 move = new Vector3(_direction.x, 0, _direction.y);
+        move.y = _verticalVelocity;
+
+        _controller.Move(move * Time.deltaTime * _speed);
     }
 }
---
+++ Assets/_Scripts/S_PushedObject.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class S_PushedObject : MonoBehaviour
+{
+    [Range(1.0f, 100.0f)]
+    [SerializeField] float _pushForce = 1.0f;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rigidbody = hit.collider.attachedRigidbody;
+
+        if (rigidbody == null || rigidbody.isKinematic)
+            return;
+
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        float force = _pushForce * hit.controller.velocity.magnitude;
+        rigidbody.AddForce(pushDirection * force, ForceMode.Impulse);
+    }
+}
